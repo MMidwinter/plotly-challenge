acb07f0625a2e64affb436d56c00f6cf7192d304
MMidwinter <75645693+MMidwinter@users.noreply.github.com>
2021-04-23 22:54:03 -0500

    Configured file paths

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
+//The code in this section is based on my instructor Doms office hours on this homework
+console.log("app.js is loaded")
+
+function InitDashboard() {
+    console.log("InitDashboard()")
+
+//Populate the drop down for Subject ID
+    var selector = d3.select("#selDataset");
+
+    d3.json("data/samples.json").then(function(data) {
+        console.log(data);
+
+         
+
+    });
+
+
+//Update the bar graph
+//Update the bubble chart
+//Update Demo Data
+
+} 
+
+InitDashboard();
+
 //Import that data from the JSON
-d3.json("./samples.json").then((importedData) => {
+//d3.json("./samples.json").then((importedData) => {
     
     
-    var data = importedData;
+   // var data = importedData;
 
 
-    var trace1 = {
-        x: data.map(row => row.samples_values),
-        y: data.map(row => row.otu_ids),
-        text: data.map(row => row.otu_labels),
-        name: "Verticle Bar",
-        type: "bar",
-        orientation: "h"
-    };
-    var chartData = [trace1];
+    //var trace1 = {
+    //    x: data.map(row => row.samples_values),
+  //      y: data.map(row => row.otu_ids),
+   //     text: data.map(row => row.otu_labels),
+   //     name: "Verticle Bar",
+   //     type: "bar",
+   //     orientation: "h"
+   // };
+  //  var chartData = [trace1];
 
-    var layout = {
-        margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 100 
-        }
-    };
-    Plotly.newPlot("plot", chartData, layout);
-});
+   // var layout = {
+   //     margin: {
+    //        l: 100,
+   //         r: 100,
+   //         t: 100,
+   //         b: 100 
+   //     }
+  //  };
+ //   Plotly.newPlot("plot", chartData, layout);
+//});
