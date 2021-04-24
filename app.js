555285704f9125be3c5bb5c6b22bbd494fde25ce
MMidwinter <75645693+MMidwinter@users.noreply.github.com>
2021-04-23 23:30:24 -0500

    bar added

--- app.js
+++ app.js
@@ -5,6 +5,40 @@ console.log("app.js is loaded")
 //Draw the Bar Chart
 function DrawBargraph(sampleId) {
     console.log(`DrawBargraph(${sampleId})`);
+
+    d3.json("data/samples.json").then(data => {
+        //console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+        
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        
+        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`); //TBD
+
+        var barData = {
+            x: sample_values.slice(0,10),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10),
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Culturs Found",
+            margin: {t:30, l:150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+
+        console.log(sample_values);
+    })
+
 }
 
 //Draw the Bubble Chart
